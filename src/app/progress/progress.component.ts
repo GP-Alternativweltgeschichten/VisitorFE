c2c3750f2dfa36985fe895588d6b96a60bb13869
Hanna Sophie Müller <mhannasophie@gmail.com>
2025-02-03 21:24:11 +0100

    fake progress bar

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,11 +1,27 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrl: './progress.component.css'
 })
-export class ProgressComponent {
+export class ProgressComponent implements OnInit{
   @Input() progress: boolean = false;
+
+  progressValue: number = 0;
+
+  ngOnInit(): void {
+    this.increaseProgress();
+  }
+
+  increaseProgress(): void {
+    const interval = setInterval(() => {
+      const increment = Math.floor(Math.random() * 5) + 1; // Zufällige Schritte von 1-5%
+      this.progressValue = Math.min(this.progressValue + increment, 99);
+      if (this.progressValue >= 99) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 
 }
