53ba655df286cdc144efae6319a00e43f7d93ab3
MReuter2 <marvin122333444455555@gmail.com>
2025-02-06 12:00:06 +0100

    fix progressbar reset bug

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,25 +1,36 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrl: './progress.component.css'
 })
-export class ProgressComponent implements OnInit{
+export class ProgressComponent implements OnChanges {
   @Input() progress: boolean = false;
 
+  interval: any;
   progressValue: number = 0;
 
-  ngOnInit(): void {
-    this.increaseProgress();
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['progress'] && this.progress) {
+      this.resetProgress();
+      this.increaseProgress();
+    }
+  }
+
+  resetProgress(): void {
+    this.progressValue = 0;
+    if (this.interval) {
+      clearInterval(this.interval); // Altes Intervall stoppen
+    }
   }
 
   increaseProgress(): void {
-    const interval = setInterval(() => {
-      const increment = Math.floor(Math.random() * 5) + 1; // Zufällige Schritte von 1-5%
+    this.interval = setInterval(() => {
+      const increment = Math.floor(Math.random() * 10) + 4; // Zufällige Schritte von 1-5%
       this.progressValue = Math.min(this.progressValue + increment, 99);
       if (this.progressValue >= 99) {
-        clearInterval(interval);
+        clearInterval(this.interval);
       }
     }, 1000);
   }
