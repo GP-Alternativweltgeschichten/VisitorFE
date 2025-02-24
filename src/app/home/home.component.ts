e0b22bc78b6f3394e94e56a7826aa341a1c68548
Hanna Sophie Müller <mhannasophie@gmail.com>
2025-02-24 17:07:54 +0100

    switch zu radiobutton

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {PromptingService} from '../services/prompting.service';
+import {RadioButtonClickEvent} from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  showModels = true;
+export class HomeComponent implements OnInit{
+  showModels: boolean = true;
+  modelOptions = [
+    { label: 'Ja', value: true },
+    { label: 'Nein', value: false }
+  ];
 
   constructor(public router: Router, public promptingService: PromptingService) {
   }
 
-  toggleModels() {
-    this.showModels = !this.showModels;
+  ngOnInit() {
+    this.showModels = this.promptingService.getShowModels();
+  }
+
+  toggleModels(event: RadioButtonClickEvent) {
+    this.showModels = event.value;
+    console.log(this.showModels)
     this.promptingService.setShowModels(this.showModels);
   }
 
